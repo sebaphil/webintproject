16061a75e304b0543bffc2b9d8d24a0ed0ec95d7
Sebastiano Filippetto <seba.fil@live.com>
2018-10-05 16:51:19 +0200

    Sexy commit message ( ͡~ ͜ʖ ͡°)

--- src/core.py
+++ src/core.py
@@ -10,7 +10,7 @@ from nltk import *
 
 
 client = MongoClient()
-# client.drop_database('stackoverflow')
+client.drop_database('stackoverflow')
 stackoverflow_database = client.stackoverflow
 questions_collection = stackoverflow_database.questions
 users_collection = stackoverflow_database.users
@@ -18,45 +18,45 @@ answers_collection = stackoverflow_database.answers
 comments_collection = stackoverflow_database.comments
 
 # PART A
-# list_of_questions_to_insert = retrieve_n_questions()
-# for list_item in list_of_questions_to_insert:
-#     questions_collection.insert_many(list_item['items'])
+list_of_questions_to_insert = retrieve_n_questions()
+for list_item in list_of_questions_to_insert:
+    questions_collection.insert_many(list_item['items'])
 
 
 questions_in_collection = questions_collection.find({})
 users_ids = []
 questions_ids = []
-# answers = []
-# answered_questions_ids = []
+answers = []
+answered_questions_ids = []
 for question in questions_in_collection:
     try:
         user_id = question['owner']['user_id']
-#         is_answered = question['is_answered']
+        is_answered = question['is_answered']
         question_id = question['question_id']
         questions_ids.append(str(question_id))
 
         if user_id not in users_ids:
             users_ids.append(str(user_id))
 
-#         if is_answered:
-#             answered_questions_ids.append(question_id)
+        if is_answered:
+            answered_questions_ids.append(question_id)
     except KeyError:
         log_retrieve_from_question(question)
 
-# number_of_retrieved_answers = 0
-# question_ids_to_use_for_request = []
-# answers_to_be_inserted = []    
-# for answered_question_id in answered_questions_ids:
-#     if number_of_retrieved_answers < 100:
-#         question_ids_to_use_for_request.append(str(answered_question_id))
-#         number_of_retrieved_answers += 1
-#     else:
-#         answers_to_be_inserted += retrieve_answers_for_questions(question_ids_to_use_for_request)['items']
-#         question_ids_to_use_for_request = []
-#         question_ids_to_use_for_request.append(str(answered_question_id))
-#         number_of_retrieved_answers = 1
+number_of_retrieved_answers = 0
+question_ids_to_use_for_request = []
+answers_to_be_inserted = []    
+for answered_question_id in answered_questions_ids:
+    if number_of_retrieved_answers < 100:
+        question_ids_to_use_for_request.append(str(answered_question_id))
+        number_of_retrieved_answers += 1
+    else:
+        answers_to_be_inserted += retrieve_answers_for_questions(question_ids_to_use_for_request)['items']
+        question_ids_to_use_for_request = []
+        question_ids_to_use_for_request.append(str(answered_question_id))
+        number_of_retrieved_answers = 1
 
-# answers_collection.insert_many(answers_to_be_inserted)
+answers_collection.insert_many(answers_to_be_inserted)
 
 
 answers_in_collection = answers_collection.find({})
@@ -114,43 +114,43 @@ for post_id in posts_ids:
             
 comments_collection.insert_many(comments_to_be_inserted)
 
-# # PART B
+# PART B
 
-# user_words_dict = {}
-# users_ids_dict = {}
-# users_in_collection = users_collection.find({})
-# for user in users_in_collection:
-#     user_words_dict[str(user['user_id'])] = []
-#     users_ids_dict[str(user['user_id'])] = 0
+user_words_dict = {}
+users_ids_dict = {}
+users_in_collection = users_collection.find({})
+for user in users_in_collection:
+    user_words_dict[str(user['user_id'])] = []
+    users_ids_dict[str(user['user_id'])] = 0
 
-# user_ids_have_questions_dict = users_ids_dict
-# questions_in_collection = questions_collection.find({})
-# for question in questions_in_collection:
-#     user_words_dict[str(question['owner']['user_id'])] += split_string_into_list_of_words(remove_all_html_tags_from_text(question['body']))
-#     user_id = question['owner']['user_id']
-#     user_ids_have_questions_dict[str(user_id)] += 1
-# print('top questioners')
-# for user_id, number_of_documents in sorted(user_ids_have_questions_dict.iteritems(), key = lambda (k,v) : (v,k)):
-#     print('%s: %s', user_id, str(number_of_documents))
+user_ids_have_questions_dict = users_ids_dict
+questions_in_collection = questions_collection.find({})
+for question in questions_in_collection:
+    user_words_dict[str(question['owner']['user_id'])] += split_string_into_list_of_words(remove_all_html_tags_from_text(question['body']))
+    user_id = question['owner']['user_id']
+    user_ids_have_questions_dict[str(user_id)] += 1
+print('top questioners')
+for user_id, number_of_documents in sorted(user_ids_have_questions_dict.iteritems(), key = lambda (k,v) : (v,k)):
+    print('%s: %s', user_id, str(number_of_documents))
 
-# users_ids_have_answers_dict = users_ids_dict
-# answers_in_collection = answers_collection.find()
-# for answer in answers_in_collection:
-#     user_words_dict[str(answer['owner']['user_id'])] += split_string_into_list_of_words(remove_all_html_tags_from_text(answer['body']))
-#     user_id = answer['owner']['user_id']
-#     users_ids_have_answers_dict[str(user_id)] += 1
-# print('top answerers')
-# for user_id, number_of_documents in sorted(users_ids_have_answers_dict.iteritems(), key = lambda (k,v) : (v,k)):
-#     print('%s: %s', user_id, str(number_of_documents))
+users_ids_have_answers_dict = users_ids_dict
+answers_in_collection = answers_collection.find()
+for answer in answers_in_collection:
+    user_words_dict[str(answer['owner']['user_id'])] += split_string_into_list_of_words(remove_all_html_tags_from_text(answer['body']))
+    user_id = answer['owner']['user_id']
+    users_ids_have_answers_dict[str(user_id)] += 1
+print('top answerers')
+for user_id, number_of_documents in sorted(users_ids_have_answers_dict.iteritems(), key = lambda (k,v) : (v,k)):
+    print('%s: %s', user_id, str(number_of_documents))
 
-# users_ids_have_comments_dict = users_ids_dict
-# comments_in_collection = comments_collection.find()
-# for comment in comments_in_collection:
-#     user_id = comment['owner']['user_id']
-#     users_ids_have_comments_dict[str(user_id)] += 1
-# print('top commenters')
-# for user_id, number_of_documents in sorted(users_ids_have_comments_dict.iteritems(), key = lambda (k,v) : (v,k)):
-#     print('%s: %s', user_id, str(number_of_documents))
+users_ids_have_comments_dict = users_ids_dict
+comments_in_collection = comments_collection.find()
+for comment in comments_in_collection:
+    user_id = comment['owner']['user_id']
+    users_ids_have_comments_dict[str(user_id)] += 1
+print('top commenters')
+for user_id, number_of_documents in sorted(users_ids_have_comments_dict.iteritems(), key = lambda (k,v) : (v,k)):
+    print('%s: %s', user_id, str(number_of_documents))
 
 
 
